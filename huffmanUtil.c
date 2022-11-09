da1b978439cd190c5226577968257d85e4b54dac
omar facchini <omar.facchini@studenti.unitn.it>
2022-11-09 16:34:20 +0100

    added the storing of the encoding of each character to a file as it will be needed when decoding

--- huffmanUtil.c
+++ huffmanUtil.c
@@ -38,27 +38,46 @@ struct maxHeapNode *buildHuffmanTree(char data[], int frequency[], int size){
     return extractMin(myMaxHeap);
 }
 
-/*prints the encoding of each character
-  takes as input the root of the tree, the array of codes and an index.
+
+/*stores the encoding of each character in a new file.
+  takes as input the array of codes, the index to reach, the character getting encoded on the file and the file itself..
 */
-void printEncoding(struct maxHeapNode *node, int code[], int top){
+void writeEncodingToFile(int array[], int size, char character, FILE *myFile){
+
+  fprintf(myFile, "%c: ", character);
+
+  int index;
+  for(index = 0; index < size; ++index){
+      fprintf(myFile,"%d", array[index]);
+  }
+  fprintf(myFile,"\n");
+
+}
+
+
+/*prints the encoding of each character
+  takes as input the root of the tree, the array of codes an index and a file to store the encodings on.
+*/
+void printEncoding(struct maxHeapNode *node, int code[], int top, FILE *myFile){
 
     //set the code to 0 for moving left.
     if(node->leftChild != NULL){
         code[top] = 0;
-        printEncoding(node->leftChild, code, top + 1);
+        printEncoding(node->leftChild, code, top + 1, myFile);
     }
 
     //set the code to 1 for moving right.
     if(node->rightChild != NULL){
         code[top] = 1;
-        printEncoding(node->rightChild, code, top + 1);
+        printEncoding(node->rightChild, code, top + 1, myFile);
     }
 
     //if the node is a leaf a character has been reached to the whole encoding is printed.
     if(isLeaf(node)){
         printf("%c: ", node->character);
         printArray(code, top);
+
+        writeEncodingToFile(code, top, node->character, myFile);
     }
 }
 
@@ -69,5 +88,8 @@ void huffmanAlgorithm(char data[], int frequency[], int size){
     struct maxHeapNode *root = buildHuffmanTree(data, frequency, size);
 
     int codes[MAX_TREE_HEIGHT], top = 0;
-    printEncoding(root, codes, top);
+    FILE *myFile;
+    myFile = fopen("encoding.txt","w");
+    printEncoding(root, codes, top, myFile);
+    fclose(myFile);
 }
--- huffmanUtil.h
+++ huffmanUtil.h
@@ -13,14 +13,19 @@ struct maxHeapNode *buildHuffmanTree(char data[], int frequency[], int size);
 
 
 /*prints the encoding of each character
-  takes as input the root of the tree, the array of codes and an index.
+  takes as input the root of the tree, the array of codes an index and a file to store the encodings on.
 */
-void printEncoding(struct maxHeapNode *node, int code[], int top);
+void printEncoding(struct maxHeapNode *node, int code[], int top, FILE *myFile);
 
 
 /*actual call to start the algorithm to build the huffman tree and to prints the encoding obtained.
   takes as input the array of characters, an array for their frequency and the size of the array.
 */
 void huffmanAlgorithm(char data[], int frequency[], int size);
+
+/*stores the encoding of each character in a new file.
+  takes as input the array of codes, the index to reach, the character getting encoded on the file and the file itself..
+*/
+void writeEncodingToFile(int array[], int size, char character, FILE *myFile);
 
 #endif
